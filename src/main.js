9a15087a62e772bf24e69640822177c4f6b73517
Barry Darmawan <waverome@gmail.com>
2017-09-14 15:44:21 +0700

    Minimize source

---
+++ src/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+          // call the tablesorter plugin
+          //$("[data-sort=table]").tablesorter({
+            // Sort on the second column, in ascending order
+            //sortList: [[1,0]]
+          //});
+        });
