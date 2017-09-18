03b5fb69eaa09047530f7e906a63ad0b7972ee5a
Barry Darmawan <waverome@gmail.com>
2017-09-18 20:33:53 +0700

    Minimize source

--- src/main.js
+++ src/main.js
@@ -1,37 +1,29 @@
 var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-    type: 'pie',
+var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Red", "Empty"],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [12, 19,],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(209, 219, 232, 0.2)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255,99,132,0.3)',
+                'rgba(209, 219, 232, 0.2)'
             ],
             borderWidth: 1
         }]
     },
     options: {
         scales: {
-            yAxes: [{
+            
                 ticks: {
                     beginAtZero: true
                 }
-            }]
+    
         }
     }
 });
